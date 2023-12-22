8c324ece0fece41d5244e49b7bdff8c94c684be0
Rahul Dhiman <rsdm34@gmail.com>
2023-12-21 20:37:27 -0800

    clerk code added

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import {Button} from "@/components/ui/button";
+
+const DashboardPage = () => {
+    return (
+        <Button variant="destructive" size="lg">Click</Button>
+    )
+}
+export default DashboardPage;
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,10 @@
+import {Button} from "@/components/ui/button";
+
+const LandingPage = () => {
+    return (
+        <div>
+            Landing Page (Unprotected)
+        </div>
+    )
+}
+export default LandingPage;
