ffc7eef7d05527a0abab1045de61e3d748862ea5
Rahul Dhiman <rsdm34@gmail.com>
2023-12-22 18:47:49 -0800

    upgrade pro dialog added according to free trial

--- app/(dashboard)/(routes)/video/page.tsx
+++ app/(dashboard)/(routes)/video/page.tsx
@@ -14,9 +14,10 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { Empty } from "@/components/empty";
 import { Loader } from "@/components/loader";
-import { cn } from "@/lib/utils";
+import { useProModal } from "@/hooks/use-pro-modal";
 
 const VideoPage = () => {
+    const proModal = useProModal();
     const router = useRouter();
     const [video, setVideo] = useState<string>();
 
@@ -39,8 +40,9 @@ const VideoPage = () => {
        
         form.reset();
        } catch (error: any) {
-        //TODO: Open Pro Model
-        console.log(error);
+           if (error?.response?.status === 403) {
+               proModal.onOpen();
+           }
        } finally {
         router.refresh();
        }
